10fcad598085c1b7f9ad61c33f2b206f1a734c0b
onkar <onkar.s.muley@gmail.com>
2021-04-24 21:44:21 +0530

    commit multiple return

--- function_basic.go
+++ function_basic.go
@@ -3,3 +3,7 @@ package main
 func Add(x, y int) int {
 	return x + y
 }
+
+func all_arith_opn(x, y int) (int, int, int, int) {
+	return x + y, x - y, x * y, x / y
+}
--- hello_world1.go
+++ hello_world1.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("5 + 6 =", Add(5, 6))
+
+	fmt.Println("Using Multiple return performing arithmatic operations")
+	fmt.Println(all_arith_opn(5, 6))
+
 }
